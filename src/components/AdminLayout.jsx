36b7df72a2de254572e156b7e0609f7ce31e43d1
moula2002 <iammoulahussain@gmail.com>
2026-01-09 20:02:55 +0530

    first commit

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -4,18 +4,20 @@ import { Outlet } from "react-router-dom";
 
 function AdminLayout() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
+    <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
+      
+      {/* Sidebar (Desktop only) */}
       <Sidebar />
 
       {/* Main Content */}
       <div className="flex-1 flex flex-col">
         <Navbar />
 
-        <div className="flex-1 p-6 overflow-y-auto">
+        <div className="flex-1 p-4 md:p-6 overflow-y-auto">
           <Outlet />
         </div>
       </div>
+
     </div>
   );
 }
